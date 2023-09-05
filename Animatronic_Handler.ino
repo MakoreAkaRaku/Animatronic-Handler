392041b2b6f427301a223e456254855ffd214012
MakoreAkaRaku <marcromancolom@gmail.com>
2023-09-06 00:25:20 +0200

    Modified new initialization with the new EyesHandler Class.

--- Animatronic_Handler.ino
+++ Animatronic_Handler.ino
@@ -13,17 +13,8 @@ void setup()
 {
     Serial.begin(9600);
     uint8_t error_code =0;
-    pair<Eye::EYE_ID,uint8_t[2]> eyes[2] ={
-        {
-            Eye::LEFT,
-            8, 9
-        },
-        {
-            Eye::RIGHT,
-            10, 11
-        }
-    };
-    if (!EyesHandler::Attach(eyes))
+    uint8_t pinIds[3] = { 2, 3, 6 };
+    if (!EyesHandler::Attach(pinIds))
     {
         error_code = 3;
     }
@@ -34,10 +25,10 @@ void setup()
         //Letting know that an error occurred.
         for (uint8_t i = 0; i < error_code; i++)
         {
-                delay(2000);
-                digitalWrite(13,HIGH);
-                delay(2000);
-                digitalWrite(13,LOW);
+            delay(2000);
+            digitalWrite(13,HIGH);
+            delay(2000);
+            digitalWrite(13,LOW);
         }
     }
 }
