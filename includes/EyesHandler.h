f27e936f3a63e259252fef85ee872659983b0415
MakoreAkaRaku <marcromancolom@gmail.com>
2023-09-06 00:24:45 +0200

    Updated Num of servos used in handler. Now class handles each axis with 1 servo. Eyelids use 1 Servo.

--- includes/EyesHandler.h
+++ includes/EyesHandler.h
@@ -1,20 +1,27 @@
 #pragma once
-#include "Eye.h"
+#include "utils.h"
+#include <Servo.h>
+
 /// @brief Class to handle the eyes movement.
 class EyesHandler
 {
+  private:
+  /// @brief
+  /// Number of Servos used to handle the entire eyes behaviour.
+  static const uint8_t NUM_OF_SERVOS_USED = 3;
   public:
   EyesHandler() = delete;
   ~EyesHandler() = delete;
-  static void MoveEyes(Eye::EYE_ID eyeIdentifier,uint16_t degrees);
-  static bool Attach(pair<Eye::EYE_ID,uint8_t[2]> eyes[2]);
+  static void MoveEyes(uint16_t degrees);
+  static bool Attach(uint8_t pinIds[NUM_OF_SERVOS_USED]);
   static bool Dettach();
-  /// @brief Returns true if the eyes handler can be used with the parameters of the constructor.
-  /// @return 
+  /// @brief Checks if handler is available.
+  /// @return true if is available, otherwise false.
   static bool IsAvailable();
   private:
-  static const uint8_t NUM_OF_EYES = 2;
   static bool isAvailable;
-  /// @brief Left eye is idx 0, right one is idx 1
-  static Eye mEyes[NUM_OF_EYES];
+  // First idx is de horizontal servo controller.
+  // Second idx is the vertical servo controller.
+  // Third idx is eyelids servo controller.
+  static Servo usedServo[NUM_OF_SERVOS_USED];
 };
--- src/EyesHandler.cpp
+++ src/EyesHandler.cpp
@@ -2,25 +2,27 @@
 #include "../includes/EyesHandler.h"
 
 bool EyesHandler::isAvailable = false;
-//Initialize with null information.
-Eye EyesHandler::mEyes[]={Eye(pair<Eye::EYE_ID,uint8_t[2]>()),pair<Eye::EYE_ID,uint8_t[2]>()}; 
+//Initializing static servos.
+Servo EyesHandler::usedServo[]={ Servo(), Servo(), Servo()}; 
 
 bool EyesHandler::IsAvailable()
 {
     return isAvailable;
 }
 
-bool EyesHandler::Attach(pair<Eye::EYE_ID,uint8_t[2]> eyes[2])
+bool EyesHandler::Attach(uint8_t pinIds[NUM_OF_SERVOS_USED])
 {
-    for (uint8_t i = 0; i < NUM_OF_EYES; i++)
+    isAvailable = true;
+    for (uint8_t i = 0; i < NUM_OF_SERVOS_USED; i++)
     {
-        mEyes[i] = Eye(eyes[i]);
-        // if the eye did not get attached
-        if(!mEyes[i].Attach())
+        //if attaching servos applied on the board exceeds the max ammount return false.
+        if( usedServo[i].attach(pinIds[i]) == MAX_SERVOS)
         {
-            return false;
+            Serial.println("EyesHandler: Maximum amount of servos already in use!");
+            isAvailable = false;
+            break;
         }
     }
     //everything is correctly attached
-    return true;
+    return isAvailable;
 }
