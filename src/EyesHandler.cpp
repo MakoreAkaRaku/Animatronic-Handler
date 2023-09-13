7bcbd03b560472e3f70510d129a49037ba4d9f94
Marc Roman Colom <marcromancolom@gmail.com>
2023-09-13 15:00:30 +0200

    Implemented MoveEyes(); amount of moves must be equal or less than actual used servos.

--- src/EyesHandler.cpp
+++ src/EyesHandler.cpp
@@ -26,3 +26,19 @@ bool EyesHandler::Attach(uint8_t pinIds[NUM_OF_SERVOS_USED])
     //everything is correctly attached
     return isAvailable;
 }
+
+void EyesHandler::MoveEyes(EyeMovement movements[])
+{
+    uint8_t numOfMoves = sizeof(movements)/sizeof(EyeMovement);
+    
+    if (numOfMoves > NUM_OF_SERVOS_USED)
+    {
+        //Evading non controlled behaviour for extended parallel movements.
+        return;
+    }
+    for (uint8_t i = 0; i < numOfMoves; i++)
+    {
+        ///TODO: Check if there's incongruencies like distinctions of NONE and VERTICAL OR HORIZONTAL.
+    }
+     
+}
